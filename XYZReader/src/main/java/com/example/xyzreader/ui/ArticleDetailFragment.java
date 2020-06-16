484b3c72f53701a3e367b468bf1d78a48f6e9c35
jose luis olivares <joselukeolivares@gmail.com>
2020-06-16 09:47:25 -0600

    Update: imageView, textView and margins
    Body: Fixed the images for cover math parent in rigth way, I added paddings and margin to titles and subtitles in cards view.

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -337,10 +337,15 @@ public class ArticleDetailFragment extends Fragment implements
                             Bitmap bitmap = imageContainer.getBitmap();
                             if (bitmap != null) {
                                 Palette p = Palette.generate(bitmap, 12);
-                                mMutedColor = p.getDarkMutedColor(0xFF333333);
+                                mMutedColor = p.getDarkMutedColor(0x00ffffff);
                                 mPhotoView.setImageBitmap(imageContainer.getBitmap());
+/*
                                 mRootView.findViewById(R.id.meta_bar)
                                         .setBackgroundColor(mMutedColor);
+
+ */
+
+
                                 updateStatusBar();
                             }
                         }
